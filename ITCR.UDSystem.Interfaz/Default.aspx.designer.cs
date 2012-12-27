2801e38a736af750ebdaf429639ebc4cecf4f792
Cesar Brenes Solano <cesarbreso@outlook.com>
2012-12-26 21:07:57 -0600

    Agregue la interfaz del CU SolicitarAreaDeportiva, solo esta el cascaron, falta conectar la funcionalidad con la interfaz, ademas corregir el editar y ponerle ver.

--- ITCR.UDSystem.Interfaz/Default.aspx.designer.cs
+++ ITCR.UDSystem.Interfaz/Default.aspx.designer.cs
@@ -38,5 +38,14 @@ namespace ITCR.UDSystem.Interfaz {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Button3;
+        
+        /// <summary>
+        /// Button4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button4;
     }
 }
